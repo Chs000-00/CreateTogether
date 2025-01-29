31067b116388debc60a10d3074d43dd45276bf75
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2025-01-29 12:38:24 -0800

    Hmmmmm SOUP

--- src/types/ActionTypes.hpp
+++ src/types/ActionTypes.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+// TODO: Fix eOption messages "not working" if optionsLayer is open.
 enum EEditorActionType {
     eActionPlacedObject,       // Object was placed in level (Implemented)
     eActionSelectedObject,     // Object was selected
@@ -12,15 +13,18 @@ enum EEditorActionType {
     eActionRequestLevel = 7,   // Request level for synchronization (Needs testing)
     eActionReturnLevelString,  // Return level string which was requested. Only host should be sending this! (Needs testing)
     eActionUpdatedColor,       // Color was updated
-    eActionModifiedObject,     // Object data was changed, used for triggers
+    // eActionModifiedObject,     // Object data was changed, used for triggers
+    eActionEnchancedObjectEdited, // Enchanced gameObject was changed
+    eActionAltGameObjectEdited,// ):
+    eActionEffectObjectEdited, // Effect gameObject was changed
     eActionSongChanged,        // Level's song was changed (Implemented)
-    eOptionSpeedChanged,       // Starting player speed was changed (Needs testing)
+    eOptionSpeedChanged,       // Starting player speed was changed (Implemented)
     eOptionGameTypeChanged,    // Classic or platformer was changed
     // Line change might be moved to another action type.
     eActionArtSelected,        // Background art, ground art, or line was changed. (Needs testing)
-    eOptionGameModeChanged,    // Starting GameMode was changed (Needs testing)
+    eOptionGameModeChanged,    // Starting GameMode was changed (Implemented)
     eActionUpdatedFont,        // Level's font was updated/changed (Implemented)
-    eActionDeleteAllStartpos,  // Delete all startpos was pressed
-    eActionLockLayer           // Host locks a layer
+    // eActionDeleteAllStartpos,  // Delete all startpos was pressed
+    eActionLockLayer,          // Host locks a layer, clients cannot unlock a layer (?)
 };
 
