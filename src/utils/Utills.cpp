d5637a8a3afe8e4c4feb322043e0f87af6f323a4
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2025-01-29 13:35:00 -0800

    a

--- src/utils/Utills.cpp
+++ src/utils/Utills.cpp
@@ -1,7 +1,10 @@
 #include "Utills.hpp"
 
 #include <Geode/Geode.hpp>
+#include "../UtilMacros.hpp"
 using namespace geode::prelude;
+
+
 
 // Checks if a enum is within range.
 // Comparison uses <=
@@ -9,37 +12,38 @@ bool isValidEnumRange(int n, int min, int max) {
     return (min <= n && n <= max);
 }
 
-// // I dont think void* would work here
-// // Nor ##T
-//
-// template <typename T>
-// Result<int> unwrapToData(void* propertyRef, matjson::Value json, const char* data) {
-//     GEODE_UNWRAP_INTO(T res, json[data].as ##T ());
-//     propertyRef = res;
-//     return Ok(0);
-// }
+template <typename T> 
+inline void setJsonObjectFromProperty(matjson::Value json, T* val, const char* str) {
+    json.set(str, val)
+}
+
+template <typename T> 
+void getJsonFromGameObject(T* obj) {
+    matjson::Value ret;
+    CREATE_TOGETHER_SET_JSON(m_duration);
+}
 
 // Todo: This
 // Returns an effectGameObject. Includes EnchancedGameObject.
-matjson::Value getJsonFromEffectGameObject(EffectGameObject* obj) {
+// matjson::Value getJsonFromEffectGameObject(EffectGameObject* obj) {
 
-}
+// }
 
-Result<EffectGameObject*> getEffectGameObjectFromMatjson(matjson::Value) {
+// Result<EffectGameObject*> getEffectGameObjectFromMatjson(matjson::Value) {
 
-}
+// }
 
-// Todo: This
-matjson::Value getJsonFromEnhancedGameObject(EnhancedGameObject* obj) {
-    matjson::Value ret;
-    ret.set("Duration", obj->m_duration)
-    ret.set("Opacity", obj->m_opacity)
-    ret.set("TargetGroupID", obj->m_targetGroupID)
-    ret.set("CenterGroupId", obj->m_centerGroupId)
-    ret.set("IsTouchTriggered", obj->m_isTouchTriggered)
+// // Todo: This
+// matjson::Value getJsonFromEnhancedGameObject(EnhancedGameObject* obj) {
+//     matjson::Value ret;
+//     ret.set("Duration", obj->m_duration)
+//     ret.set("Opacity", obj->m_opacity)
+//     ret.set("TargetGroupID", obj->m_targetGroupID)
+//     ret.set("CenterGroupId", obj->m_centerGroupId)
+//     ret.set("IsTouchTriggered", obj->m_isTouchTriggered)
 
-}
+// }
 
-Result<EnhancedGameObject*> getEnhancedGameObjectFromMatjson(matjson::Value) {
+// Result<EnhancedGameObject*> getEnhancedGameObjectFromMatjson(matjson::Value) {
 
-}
+// }
