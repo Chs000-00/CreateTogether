9c80f145c1fc73fdee3e77f069fff36a4c9a1cc7
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2025-01-29 08:45:43 -0800

    assssss WEB EDITOR

--- src/utils/Utills.hpp
+++ src/utils/Utills.hpp
@@ -1,2 +1,14 @@
 
 bool isValidEnumRange(int n, int min, int max);
+
+
+// Todo: This
+// Returns an effectGameObject. Includes EnchancedGameObject.
+matjson::Value getJsonFromEffectGameObject(EffectGameObject* obj);
+
+Result<EffectGameObject*> getEffectGameObjectFromMatjson(matjson::Value);
+
+// Todo: This
+matjson::Value getJsonFromEnhancedGameObject(EnhancedGameObject* obj);
+
+Result<EnhancedGameObject*> getEnhancedGameObjectFromMatjson(matjson::Value);
