daca76558507b66c30df8821ef59312cc11ab140
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2025-02-01 18:05:38 -0800

    template stuff

--- src/utils/Utills.hpp
+++ src/utils/Utills.hpp
@@ -7,32 +7,43 @@ using namespace geode::prelude;
 
 bool isValidEnumRange(int n, int min, int max);
 
-
-template <typename T> 
+// TODO: Place this stuff in another file
+template < typename T > 
 void setJsonObjectFromProperty( T val, matjson::Value json, const char* str) {
     json.set(str, val);
 }
 
-template <typename T> 
+template < typename T > 
 void setJsonObjectFromProperty( T* val, matjson::Value json, const char* str) {
     json.set(str, val);
 }
 
 
-// TODO: Uhh this I guess
-template <typename T> 
+template < typename T > 
 matjson::Value getJsonFromGameObject(T* obj) {
-    matjson::Value ret;
-    CREATE_TOGETHER_SET_JSON(m_duration);
-    CREATE_TOGETHER_SET_JSON(m_opacity);
-    //setJsonObjectFromProperty(obj->m_opacity, ret, "m_opacity");
 
+    static_assert(std::is_base_of<GameObject, T>::value,"T must be a derived class of GameObject in getJsonFromGameObject<T>.");
+
+    matjson::Value ret;
+
+    if (std::is_base_of<EffectGameObject, T>::value) {
+        CREATE_TOGETHER_SET_JSON(m_duration);
+        CREATE_TOGETHER_SET_JSON(m_opacity);
+        CREATE_TOGETHER_SET_JSON(m_targetGroupID);
+        CREATE_TOGETHER_SET_JSON(m_centerGroupID);
+        CREATE_TOGETHER_SET_JSON(m_isTouchTriggered);
+        CREATE_TOGETHER_SET_JSON(m_isSpawnTriggered);
+        CREATE_TOGETHER_SET_JSON(m_hasCenterEffect);
+        CREATE_TOGETHER_SET_JSON(m_shakeStrength);
+        CREATE_TOGETHER_SET_JSON(m_shakeInterval);
+    }
 
 
     return ret;
 }
 
-template <typename T> 
+// TODO: Uhh this I guess
+template < typename T > 
 matjson::Value setGameObjectFromJson(T* obj) {
     matjson::Value ret;
 
