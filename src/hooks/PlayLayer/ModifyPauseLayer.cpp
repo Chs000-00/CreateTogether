bfd7e6a954937d2f0d09a20bfaa59e88c9fb0b85
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2025-02-19 19:20:59 -0800

    I wrote more stuff multiple times already

--- src/hooks/PlayLayer/ModifyPauseLayer.cpp
+++ src/hooks/PlayLayer/ModifyPauseLayer.cpp
@@ -2,15 +2,32 @@
 #include <Geode/modify/PauseLayer.hpp>
 #include "ModifyPlayLayer.hpp"
 #include "ModifyPauseLayer.hpp"
-
+#include "../ModifyGameManager.hpp"
 
 using namespace geode::prelude;
 
 void MyPauseLayer::onQuit(CCObject* sender) {
-    // auto playLayer = static_cast<MyPlayLayer*>(PlayLayer::get())
-    PauseLayer::onQuit(sender);
+    auto playLayer = static_cast<MyPlayLayer*>(PlayLayer::get());
+    auto gameManager = static_cast<MyGameManager*>(GameManager::get());
+
+    if (playLayer->m_fields->m_isFakeLayer) {
+        removeFromParent();
+        playLayer->removeFromParent();
+        gameManager->leaveLobby();
+        // More stuff here im too lazy to implement
+    }
+    else {
+        PauseLayer::onQuit(sender);
+    }
 }
 
 void MyPauseLayer::onEdit(CCObject* sender) {
-    PauseLayer::onEdit(sender);
+    auto playLayer = static_cast<MyPlayLayer*>(PlayLayer::get());
+    if (playLayer->m_fields->m_isFakeLayer) {
+        removeFromParent();
+        playLayer->removeFromParent();
+    }
+    else {
+        PauseLayer::onQuit(sender);
+    }
 }
