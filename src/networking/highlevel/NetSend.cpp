37d7502f844475f1529a324b47b1d45fd6fbfe5e
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2025-05-17 23:34:34 -0700

    Idk

--- src/networking/highlevel/NetSend.cpp
+++ src/networking/highlevel/NetSend.cpp
@@ -1,6 +1,15 @@
 #include "../HighLevelHeader.hpp"
 #include "SharedHighLevelHeaders.hpp"
 
+void sendCreateObjects(const char* uniqueID, CCPoint pos, float rotation, bool isHighDetail, bool noGlow, bool noEnter, bool flipX, bool flipY) {
+    flatbuffers::FlatBufferBuilder builder(512);
+
+    auto objectPos = CTSerialize::CCPosI(pos.x, pos.y);
+
+    auto minObj = CTSerialize::CreateGDGameObjectMinDirect(builder, uniqueID, &objectPos, rotation, isHighDetail, noGlow, noEnter); //uhh add stuff here
+
+}
+
 void sendMoveObjects(std::vector<const char*> uniqueIDList, CCPoint offset) {
 
 }
--- src/networking/highlevel/SharedHighLevelHeaders.hpp
+++ src/networking/highlevel/SharedHighLevelHeaders.hpp
@@ -2,4 +2,7 @@
 
 #include <Geode/Geode.hpp>
 
+#include <flatbuffers/flatbuffers.h>
+#include <ctserialize_generated.h>
+
 using namespace geode::prelude;
