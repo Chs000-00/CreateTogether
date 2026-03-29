3b4f0af67e8ef5c2466ee5f60f71da5b5f04b89f
Igbalabanov <101612094+Chs000-00@users.noreply.github.com>
2026-03-28 17:54:44 -0700

    Boilerplate

---
+++ include/createtogether/Networking/NetworkingBackends/HasCapableBackend.hpp
@@ -0,0 +1,13 @@
+// Fully virtual class containing the following:
+
+// sendMessageToLobby(); - Sends a flatbuf. message to everyone else in the lobby
+// recvMessages(); - Recvs. a list of queued messages to parse
+
+// Possibly?
+// sendCursorMessage()
+// recvCursorMessage()
+
+// (?) startHostingServer(); - Is able to start hosting server
+
+// TODO: Implement a way to identify users
+// std::vector<UserIdentity> m_excludedMemberList;
--- src/networking/HighLevelHeader.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "highlevel/NetRecv.hpp" // IWYU pragma: export
-#include "highlevel/NetSend.hpp" // IWYU pragma: export
-
